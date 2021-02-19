126b74afb142e088e5139c501c380b7c30437fb1
alexteo98 <alexteo98@gmail.com>
2021-02-19 19:49:00 +0800

    redid Box.java for understanding

--- lab4-alexteo98/Box.java
+++ lab4-alexteo98/Box.java
@@ -1,53 +1,46 @@
-class Box<T> { 
-  private final T item;
-  private final static Box<?> EMPTY_BOX = new Box(null);
+class Box<T>  { 
+    private final T itemInside;
+    public static <S> Box<S super T> EMPTY_BOX = new Box<S> (null);
 
-  private Box(T item) { 
-    this.item = item;
-  }
-
-  public static <S> Box<S> of(S item) { 
-    if (item != null) { 
-      return new Box(item);
-    } else { 
-      return null;
+    private Box(T item) { 
+        this.itemInside = item;
     }
-  }
 
-  public static <S> Box<S> ofNullable(S item) { 
-    if (item != null) { 
-      return new Box(item);
-    } else { 
-      return (Box<S>) empty();
-    }
-  } 
-
-  public static <X> Box<X> empty() { 
-    return (Box<X>)EMPTY_BOX;
-  }
-
-  public boolean isPresent() { 
-    Box compare1 = new Box(this.item);
-    Box compare2 = empty();
-    if (compare1.equals(compare2)) { 
-          return true;
-      } else { 
-          return false;
+    public static <S> Box of(S item) {
+      if (item == null) { 
+          return null;
+      } else {
+        return new Box(item);
       }
-  }
-
-  @Override
-  public boolean equals(Object item) { 
-    if (item instanceof Box) { 
-      Box<T> comparator = (Box<T>) item;
-      return this.item.equals(comparator.item);
-    } else { 
-      return false;
     }
-  }
 
-  @Override
-  public String toString() { 
-    return String.format("[%s]", item);
-  }
+    public static <S> Box ofNullable(S item) {
+      if (item == null) { 
+          return null;
+      } else {
+        return empty();
+      }
+    }
+    public static <S> Box empty() { 
+        return (Box<T>) EMPTY_BOX;
+    }
+
+    @Override
+    public boolean equals(Object item) { 
+        if (item instanceof Box) { 
+            Box<T> comparator = (Box<T>) item;
+            return this.itemInside.equals(comparator.itemInside);
+        } else { 
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() { 
+      String str = "";
+      if (this.itemInside != null) { 
+          str = itemInside.toString();
+      }
+        return String.format("[%s]", str);
+    }
 }

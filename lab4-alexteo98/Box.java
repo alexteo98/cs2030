4f2000f4b9f401cb412a385fe1cd784dbb4d07a2
alexteo98 <alexteo98@gmail.com>
2021-02-19 22:42:16 +0800

    halfway q2

--- lab4-alexteo98/Box.java
+++ lab4-alexteo98/Box.java
@@ -1,46 +1,58 @@
 class Box<T>  { 
-    private final T itemInside;
-    public static <S> Box<S super T> EMPTY_BOX = new Box<S> (null);
+  private final T itemInside;
+  public static <S> Box<? super S> EMPTY_BOX = new Box<S> (null);
 
-    private Box(T item) { 
-        this.itemInside = item;
-    }
+  private Box(T item) { 
+    this.itemInside = item;
+  }
 
-    public static <S> Box of(S item) {
-      if (item == null) { 
-          return null;
-      } else {
-        return new Box(item);
-      }
+  public static <S> Box of(S item) {
+    if (item == null) { 
+      return null;
+    } else {
+      return new Box(item);
     }
+  }
 
-    public static <S> Box ofNullable(S item) {
-      if (item == null) { 
-          return null;
-      } else {
-        return empty();
-      }
+  public static <S> Box ofNullable(S item) {
+    if (item == null) { 
+      return null;
+    } else {
+      return empty();
     }
-    public static <S> Box empty() { 
-        return (Box<T>) EMPTY_BOX;
-    }
+  }
 
-    @Override
-    public boolean equals(Object item) { 
-        if (item instanceof Box) { 
-            Box<T> comparator = (Box<T>) item;
-            return this.itemInside.equals(comparator.itemInside);
-        } else { 
-            return false;
-        }
+  public boolean isPresent() { 
+    Box<T> temp = (Box<T>) EMPTY_BOX;
+    if (this.equals(temp)) { 
+        return false;
+    } else { 
+        return true;
     }
+  }
 
-    @Override
-    public String toString() { 
-      String str = "";
-      if (this.itemInside != null) { 
-          str = itemInside.toString();
-      }
-        return String.format("[%s]", str);
+  public static <S> Box<S> empty() { 
+    // Null Box can be casted into any box.
+    @SuppressWarnings("unchecked")
+    return (Box<S>) EMPTY_BOX;
+  }
+
+  @Override
+  public boolean equals(Object item) { 
+    if (item instanceof Box) { 
+      Box<T> comparator = (Box<T>) item;
+      return this.itemInside.equals(comparator.itemInside);
+    } else { 
+      return false;
     }
+  }
+
+  @Override
+  public String toString() { 
+    String str = "";
+    if (this.itemInside != null) { 
+      str = itemInside.toString();
+    }
+    return String.format("[%s]", str);
+  }
 }

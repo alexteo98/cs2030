f2772da71a4cb36b23a926bb963dd0d6955d22e3
alexteo98 <alexteo98@gmail.com>
2021-02-20 19:55:15 +0800

    lab4 pt 2 passed

--- lab4-alexteo98/Box.java
+++ lab4-alexteo98/Box.java
@@ -1,47 +1,52 @@
 class Box<T>  { 
   private final T itemInside;
-  public static <S> Box<? super S> EMPTY_BOX = new Box<S> (null);
+  public static final Box<?> EMPTY_BOX = new Box(null);
 
   private Box(T item) { 
     this.itemInside = item;
   }
 
-  public static <S> Box of(S item) {
+  public static <S> Box<S> of(S item) {
     if (item == null) { 
       return null;
     } else {
-      return new Box(item);
+      return (Box<S>)new Box<S>(item);
     }
   }
 
-  public static <S> Box ofNullable(S item) {
+  public static <S> Box<S> ofNullable(S item) {
     if (item == null) { 
-      return null;
-    } else {
       return empty();
+    } else {
+      return (Box<S>) new Box<S>(item);
     }
   }
 
   public boolean isPresent() { 
-    Box<T> temp = (Box<T>) EMPTY_BOX;
-    if (this.equals(temp)) { 
-        return false;
+    Box<T> temp = new Box(this.itemInside);
+    if (temp.equals(EMPTY_BOX)) { 
+      return false;
     } else { 
-        return true;
+      return true;
     }
   }
 
   public static <S> Box<S> empty() { 
     // Null Box can be casted into any box.
     @SuppressWarnings("unchecked")
-    return (Box<S>) EMPTY_BOX;
+    Box<S> temp = (Box<S>) EMPTY_BOX;
+    return temp;
   }
 
   @Override
   public boolean equals(Object item) { 
     if (item instanceof Box) { 
       Box<T> comparator = (Box<T>) item;
-      return this.itemInside.equals(comparator.itemInside);
+      if (this.itemInside == null) { 
+        return this.itemInside == comparator.itemInside;
+      } else{
+        return this.itemInside.equals(comparator.itemInside);
+      }
     } else { 
       return false;
     }

57b6ebd80fa5cf66289f368db28f4df3b831e3bc
Alex Teo <alexteo98@gmail.com>
2021-02-27 04:14:44 +0800

    added checkstyle default command

--- checkstyle.sh
+++ checkstyle.sh
@@ -1,3 +1,5 @@
 #! /bin/sh
 
 java -jar ~/university/cs2030/checkstyle/checkstyle.jar -c ~/university/cs2030/checkstyle/cs2030_checks.xml *.java
+
+# java -jar ~cs2030/bin/checkstyle.jar -c ~cs2030/bin/cs2030_checks.xml *.java

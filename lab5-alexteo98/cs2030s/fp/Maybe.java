00c58d8183b8cb83f56a50b9a495075c42be04dc
alexteo98 <alexteo98@gmail.com>
2021-03-11 07:12:24 -0800

    passed half of test 1

--- lab5-alexteo98/cs2030s/fp/Maybe.java
+++ lab5-alexteo98/cs2030s/fp/Maybe.java
@@ -8,19 +8,48 @@ package cs2030s.fp;
  */
 
 public abstract class Maybe<T> { 
-    
-}
+ 
+  public static <S> Maybe<S> of(S item) { 
+      if (item==null) { 
+          return some(item);
+      } else  { 
+          return none();
+      }
+  }
+  public static <S> Some<S> some(S item) { 
+      return new Some(item);
+  }
 
-public static final class None extends Maybe<Object> { 
-  
+  public static <S> Maybe<S> none() { 
+      return (Maybe<S>) NONE;
+  }
 
+  private static final Maybe<?> NONE = new None();
+  //}
+
+  public static final class None extends Maybe<Object> { 
+
+    private Object item;
+
+    public None() { 
+      this.item =  null;
+    }
+
+    @Override
+    public String toString() { 
+      return "[]";
+    }
+  }
+
+public static final class Some<T> extends Maybe<T> { 
+  private T item;
+  public Some(T t) { 
+    this.item = t;
+  }
 
   @Override
   public String toString() { 
-      return "[]";
+    return String.format("[%s]", this.item);
   }
 }
-
-public static final class Some<T> extends Maybe<T> { 
-    
 }

05e3704d34ccb22b8b3b5ad606346a8cb19b7fef
Alex Teo <alexteo98@gmail.com>
2021-03-11 18:23:59 +0800

    added lab 5 again

---
+++ lab5-alexteo98/cs2030s/fp/Maybe.java
@@ -0,0 +1,26 @@
+package cs2030s.fp;
+
+/**
+ * CS2030S Lab 5
+ * AY20/21 Semester 2
+ *
+ * @author Alex Teo (Lab 16A)
+ */
+
+public abstract class Maybe<T> { 
+    
+}
+
+public static final class None extends Maybe<Object> { 
+  
+
+
+  @Override
+  public String toString() { 
+      return "[]";
+  }
+}
+
+public static final class Some<T> extends Maybe<T> { 
+    
+}

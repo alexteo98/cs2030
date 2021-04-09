8f2f7917490ee7ee62af4db24d7fe938dfe5789f
alexteo98 <alexteo98@gmail.com>
2021-04-09 10:13:19 -0700

    edited maybe and lazy for printing

--- lab7-alexteo98/cs2030s/fp/Lazy.java
+++ lab7-alexteo98/cs2030s/fp/Lazy.java
@@ -1,136 +1,53 @@
 package cs2030s.fp;
 
-/**
- * A wrapper around a Maybe object that might contain 
- * an item or might not. Does not evaluate the Producer until called upon.
- *
- * @author Alex Teo (Lab 16A)
- * @version CS2030S AY 20/21 Sem2
- */
 public class Lazy<T> {
 
-  /** A producer of type T to produce the element. */
   private Producer<T> producer;
 
-  /** The wrapped item Maybe that might or might not contain an item. */
   private Maybe<T> value = Maybe.none();
 
-  /** 
-   * Factory method to create the Lazy object using an item of type T.
-   *
-   * @param <T> The type of item to be wrapped.
-   * @param v Item of type t to be wrapped with a Maybe object.
-   * @return A Lazy object wrapped around a Maybe object of the parameter.
-   */
   public static final <T> Lazy<T> of(T v) {  
     return new Lazy<T>(v);
   }
 
-  /**
-   * Private Constructor for Lazy Object.
-   * Wraps given argument in a Maybe object.
-   *
-   * @param v The item to be wrapped.
-   */
   private Lazy(T v) { 
     this.value = Maybe.of(v);
   }
 
-  /**
-   * Factory method to create the Lazy object using an item of type T
-   * using a producer of type T.
-   *
-   * @param <T> The type of item to be wrapped.
-   * @param v Producer of type T to be evaluated later.
-   * @return A Lazy object with the producer stored.. 
-   */
   public static final <T> Lazy<T> of(Producer<T> v) { 
     return new Lazy<T>(v);
   }
 
-  /**
-   * Private Contructor for Lazy Object.
-   * Stores the given producer to be evaluated later.
-   *
-   * @param v The producer to be stored and evaluated later.
-   */
   private Lazy(Producer<T> v) { 
     this.producer = v;
   }
 
-  /**
-   * Returns the stored value.
-   * Checks if there is a value cached in the Maybe Object.
-   * Evaluates the producer if it is empty and caches it.
-   *
-   * @return The value stored or evaluated.
-   */
   public T get() { 
     this.value = Maybe.some(this.value.<T>orElseGet(this.producer));
     return this.value.get();
   }
 
-  /**
-   * Maps the current value to a new value using a Transformer.
-   * The function is stored as a producer and is only evaluated when called by get().
-   *
-   * @param <U> The type that the transformer transforms this to.
-   * @param t The Transformer function to be applied to the value.
-   * @return A Lazy object that stores the evaluation.
-   */
   public <U> Lazy<U> map(Transformer<? super T, ? extends U> t) {  
     Producer<U> p = () -> t.transform(this.get());
     return Lazy.<U>of(p);
   }
 
-  /**
-   * Maps the current value to a new value using a Transformer.
-   * The function is stored as a producer and is only evaluated when called by get().
-   * Instead of returning a value wrapped around twice, returns the producer wrapped only once.
-   *
-   * @param <U> The type that the transformer transforms this to.
-   * @param t The Transformer function to be applied to the value.
-   * @return A Lazy object that stores the evaluation but only wraps it once.
-   */
   public <U> Lazy<U> flatMap(Transformer<? super T, ? extends Lazy<? extends U>> t) { 
     Producer<U> p = () -> t.transform(this.get()).get();  
     return Lazy.<U>of(p);
   }
 
-  /**
-   * Filters the contents of this.
-   * Stores the operation in a Lazy boolean object but only evaluates it when called upon.
-   *
-   * @param bc BooleanCondition to test the contents of this against.
-   * @return Lazy of type boolean of the result of the test.
-   */
   public Lazy<Boolean> filter(BooleanCondition<? super T> bc) { 
     Producer<Boolean> p = () -> bc.test(this.get());
     return Lazy.<Boolean>of(p);
   }
 
-  /**
-   * Combines this with another instance of Lazy. Given Lazy(y), 
-   * stores an operation f(x, y) in a producer and wraps it with an Lazy Object.
-   *
-   * @param <S> The type of the other Lazy object.
-   * @param <R> The type to be returned after calling f(x, y).
-   * @param lazy The other lazy object to combime with.
-   * @param cmb Combiner to combine the two Lazy objects with.
-   * @return A Lazy Object wrapped around the producer of f(x, y).
-   */
   public <S, R> Lazy<R> combine(Lazy<? extends S> lazy, 
       Combiner<? super T, ? super S, ? extends R> cmb) { 
     Producer<R> p = () -> cmb.combine(this.get(), lazy.get());
     return Lazy.<R>of(p);
   }
 
-  /**
-   * Comapares this item with the argument given.
-   *
-   * @param o An object to compare this item to.
-   * @return True if the items are equal according to their equals representation, False otherwise.
-   */
   @Override
   public boolean equals(Object o) { 
     if (o instanceof Lazy) { 
@@ -143,11 +60,6 @@ public class Lazy<T> {
     }
   }
 
-  /**
-   * Return the string representation of the item inside.
-   *
-   * @return The string representation of the item inside.
-   */
   @Override
   public String toString() {  
     return this.value.map(String::valueOf).orElse("?");
--- lab7-alexteo98/cs2030s/fp/Maybe.java
+++ lab7-alexteo98/cs2030s/fp/Maybe.java
@@ -2,32 +2,11 @@ package cs2030s.fp;
 
 import java.util.NoSuchElementException;
 
-/**
- * This abstract class implements an object of Maybe, whereby it could contain an element,
- * "Some" or it could not contain en element, "None".
- *
- * <p>CS2030S Lab 5
- * AY20/21 Semester 2
- *
- * @author Alex Teo (Lab 16A) 
- */
 public abstract class Maybe<T> { 
 
-  /**
-   * Abstract method to get the element inside of its subtypes Some and None.
-   *
-   * @return Element T inside of Some.
-   * @throws NoSuchElementException When called on instance of None.
-   */
   protected abstract T get();
 
-  /**
-   * Factory method to create an item wrapped around Maybe.
-   *
-   * @param <S> The type of object to be wrapped.
-   * @param item The item of type S to be wrapped.
-   * @return None Object if given Null, Some Object otherwise.
-   */
+
   public static <S> Maybe<S> of(S item) { 
     if (item == null) { 
       return none();
@@ -36,59 +15,20 @@ public abstract class Maybe<T> {
     }
   }
 
-  /**
-   * Calls the constructor of Some with type S.
-   *
-   * @param <S> The type of item to be wrapped.
-   * @param item of type S to be wrapped with an instance of Some.
-   * @return An instance of Some wrapped around an object of type S.
-   */
   public static <S> Some<S> some(S item) { 
     return new Some<S>(item);
   }
 
-  /**
-   * Returns a static reference to a None Object casted to Maybe of type S.
-   *
-   * @param <S> Type of Maybe NONE should be casted to.
-   * @return None object casted to type S.
-   */
   public static <S> Maybe<S> none() { 
     @SuppressWarnings("unchecked")
     Maybe<S> temp = (Maybe<S>) NONE;
     return temp;
   }
 
-  /**
-   * Abstract Method to get element T if it is an instance of Some, returns the parameter otherwise.
-   *
-   * @param <U> The type to be returned that must be a subtype of T.
-   * @param u element which is a subtype of T.
-   * @return The element of type T if it is an instance of Some, 
-    The parameter of type U if it is an instance of None.
-   */
   public abstract <U extends T> T orElse(U u);
 
-  /**
-   * Abstract Method to get element T if it is an instance of Some, 
-   * returns the element produced by argument otherwise.
-   *
-   * @param <U> The type to be returned that must be a subtype of T.
-   * @param p Producer to produce an element if it is an instance of None.
-   * @return The element of type T if it is an instance of Some, 
-    otherwise the element of type U produced by parameter p if it is an instance of None.
-   */
   public abstract <U extends T> T orElseGet(Producer<U> p);
 
-  /**
-   * Changes the current item of type T into type U with a Transformer.
-   * Transformer must have a type of ? super T, ? extends U.
-   *
-   * @param <U> The type of Maybe to be returned after transforming.
-   * @param t Transformer to change current element of type T to element of type U.
-   * @return An instance of None if used on an instance of None, or
-    An instance of Some wrapped around the item transformed by the transformer argument.
-   */
   public <U> Maybe<U> map(Transformer<? super T, ? extends U> t) {  
     if (this == NONE) { 
       return none();
@@ -97,16 +37,6 @@ public abstract class Maybe<T> {
     }
   }
 
-  /**
-   * Changes the current item of type T into type U with a Transformer.
-   * Transforms the element of type T into type U such that it does not get wrapped twice.
-   * Transformer must have a type of (? super T, ? extends Maybe(? extends U)).
-   *
-   * @param <U> The type if Maybe to be returned after transforming.
-   * @param t Transformer to change current element of type T to element of type U.
-   * @return An instance of None if used on an instance of None. or
-   An instance of Some wrapped around the item transformed by the transformer argument.
-   */
   public <U> Maybe<U> flatMap(Transformer<? super T, ? extends Maybe<? extends U>> t) { 
     if (this == NONE) { 
       return none();
@@ -117,18 +47,8 @@ public abstract class Maybe<T> {
     }
   }
 
-  /**
-   * Static None object that all None instances refer to.
-   */
   private static final Maybe<Object> NONE = new None();
-
-  /**
-   * Tests the element inside with the given BooleanCondition.
-   *
-   * @param bc Boolean Condition Object to test the element with.
-   * @return None object if the element fails the test or this is an instance of None, .
-   Otherwise the original object if it passes the test.
-   */
+ 
   public Maybe<T> filter(BooleanCondition<? super T> bc) { 
     Maybe<T> temp = none();  
     if (this == NONE) { 
@@ -144,126 +64,59 @@ public abstract class Maybe<T> {
     }
   }
 
-
-  /**
-   * This class implements a None object, whereby it does not contain anything inside.
-   */
   public static final class None extends Maybe<Object> { 
 
-    /**
-     * Does not retuan an item, throws a NoSuchElementException.
-     *
-     * @throws NoSuchElementException As None does not have an element inside.
-     */
     @Override
     protected Object get() throws NoSuchElementException { 
       throw new NoSuchElementException();
     }
 
-    /**
-     * Private constrcutor for None, does nothing.
-     */
     private None() {
     }
 
-    /**
-     * Does nothing and returns the element U given as parameter.
-     *
-     * @param u The element to return.
-     * @return u The element returned.
-     */
     @Override
     public <U> U orElse(U u) { 
       return u;
     }
 
-    /**
-     * Does nothing and returns the element produced by the producer given as the parameter.
-     *
-     * @param p The Producer of type U that will be executed.
-     * @return p The element of type U produced by the producer.
-     */
     @Override
     public <U> U orElseGet(Producer<U> p) { 
       return p.produce();
     }
 
-    /**
-     * Compares if this is equal to the Maybe object given.
-     *
-     * @param compareTo the Maybe Object to be compared to.
-     * @return True if compareTo is a None object, False otherwise.
-     */
     public boolean equals(Maybe<Object> compareTo) { 
       return compareTo instanceof None;
     }
 
-    /**
-     * Returns the String representation of None.
-     *
-     * @return Always return "[]" as None has nothing inside.
-     */
     @Override
     public String toString() { 
       return "[]";
     }
   }
 
-  /**
-   * This class implements a Some object, whereby it contains an item of type T inside.
-   */
   public static final class Some<T> extends Maybe<T> { 
 
-    /** The item of type T that is inside Some. */
     private T item;
 
-    /**
-     * Private Constructor of Some. Initialises the item inside with the parameter.
-     *
-     * @param t Item of type T the be wrapped by the Some object.
-     */
     private Some(T t) { 
       this.item = t;
     }
 
-    /**
-     * Gets the item inside.
-     *
-     * @return The item inside the Some object.
-     */
     @Override
     protected T get() { 
       return this.item;
     }
 
-    /**
-     * Gets the item inside.
-     *
-     * @param u The item u to be returned if this is a none object.(Not used)
-     * @return The item inside the Some object.
-     */   
     @Override
     public <U extends T> T orElse(U u) { 
       return this.get();
     }
 
-    /**
-     * Gets the item inside.
-     *
-     * @param p The producer to produce an item of type U if this is a none object.(Not used)
-     * @return The item inside the Some object.
-     */
     @Override
     public <U extends T> T orElseGet(Producer<U> p) { 
       return this.get();
     }
 
-    /**
-     * Compares if the item is equal to this.
-     *
-     * @param compareTo The item to be compared to.
-     * @return True if the item are equal according to their equals implementation, False otherwise.
-     */
     @Override
     public boolean equals(Object compareTo) { 
       if (compareTo instanceof Some) { 
@@ -279,11 +132,6 @@ public abstract class Maybe<T> {
       }
     }
 
-    /**
-     * Gets the String representation of the item inside.
-     *
-     * @return String representation of the item inside.
-     */
     @Override
     public String toString() { 
       return String.format("[%s]", this.item);
